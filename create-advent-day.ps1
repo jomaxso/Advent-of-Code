5a501e364834f742f8efc1f72f0a4a065c541680
Johannes-Max Sorge <jomaxso@outlook.com>
2024-11-28 18:53:44 +0100

    test workflow

--- create-advent-day.ps1
+++ create-advent-day.ps1
@@ -12,11 +12,11 @@ $slnPath = "$srcPath\$slnName.sln"
 $folderName = "Day{0:D2}" -f $currentDay
 $fullPath = "$srcPath\$folderName"
 
-if ((Get-Date) -lt (Get-Date -Year $currentYear -Month 12 -Day 01) -or (Get-Date) -lt (Get-Date -Year $currentYear -Month 12 -Day 25))
-{
-    Write-Host "Advent of Code $currentYear has not started yet. Please try again on December 1st."
-    exit 1
-}
+#if ((Get-Date) -lt (Get-Date -Year $currentYear -Month 12 -Day 01) -or (Get-Date) -lt (Get-Date -Year $currentYear -Month 12 -Day 25))
+#{
+#    Write-Host "Advent of Code $currentYear has not started yet. Please try again on December 1st."
+#    exit 1
+#}
 
 # Create src root directory if it doesn't exist
 if (-not (Test-Path -Path $srcRoot))
