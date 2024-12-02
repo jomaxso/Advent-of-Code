3134b64d9e01bfc7235cd7502640afa06c7c2f2c
johannes-max.sorge <johannes-max.sorge@deutschepost.de>
2024-12-02 15:52:40 +0100

    day-02: resolved daily quest

--- src/2024/Day02/Solution/Program.cs
+++ src/2024/Day02/Solution/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using Solution;
+
+var stamp = Stopwatch.GetTimestamp();
+
+var lineCount = 0;
+var safeReports = 0;
+
+await foreach (var reportString in File.ReadLinesAsync("input.txt"))
+{
+    lineCount++;
+    if (Report.IsSafe(reportString))
+    {
+        safeReports++;
+    }
+}
+
+var elapsed = Stopwatch.GetTimestamp() - stamp;
+
+Console.WriteLine($"Elapsed: {TimeSpan.FromTicks(elapsed).Milliseconds} ms");
+Console.WriteLine($"Safe reports: {safeReports} out of {lineCount}");
---
+++ src/2024/Day02/Solution/Report.cs
@@ -0,0 +1,48 @@
+﻿namespace Solution;
+
+public static class Report
+{
+    public static bool IsSafe(ReadOnlySpan<char> report)
+    {
+        var reportSpan = report.Trim();
+        var spaceCount = reportSpan.Count(' ');
+
+        Span<int> levels = stackalloc int[spaceCount + 1];
+
+        var index = 0;
+        foreach (var range in reportSpan.Split(' '))
+        {
+            levels[index] = int.Parse(reportSpan[range]);
+            index++;
+        }
+
+        return IsSafe(levels);
+    }
+
+    public static bool IsSafe(ReadOnlySpan<int> report)
+    {
+        if (report.Length < 2)
+            return true;
+
+        var previousLevel = report[0];
+        var isIncreasing = report[1] > previousLevel;
+
+        for (var i = 1; i < report.Length; i++)
+        {
+            var level = report[i];
+
+            var isSafe = (level - previousLevel) switch
+            {
+                1 or 2 or 3 when isIncreasing => true,
+                -1 or -2 or -3 when !isIncreasing => true,
+                _ => false
+            };
+
+            if (!isSafe) return false;
+
+            previousLevel = level;
+        }
+
+        return true;
+    }
+}
