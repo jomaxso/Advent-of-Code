9cd73f35bb62d497094ade434abd2eda14590368
Johannes-Max Sorge <55972873+jomaxso@users.noreply.github.com>
2024-12-05 18:47:27 +0100

    Merge pull request #16
    * Automated commit for new Advent Day on 2024-12-04. See issue #.
    
    * day-4: solve puzzle

---
+++ src/2024/Day04/Test/XSearchTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Solution;
+
+namespace Test;
+
+public sealed class XSearchTests
+{
+    private const string TestInput = """
+                                     .M.S......
+                                     ..A..MSMS.
+                                     .M.S.MAA..
+                                     ..A.ASMSM.
+                                     .M.S.M....
+                                     ..........
+                                     S.S.S.S.S.
+                                     .A.A.A.A..
+                                     M.M.M.M.M.
+                                     ..........
+                                     """;
+
+    [Fact]
+    public void Count_GridWithEvenWidth_ThrowsArgumentException()
+    {
+        const string word = "XMAS";
+        
+        var func = () =>
+        {
+            var grid = new Grid<char>(TestInput.Replace("\n", ""), 10);
+            return XSearch.Count(grid, word);
+        };
+
+        func.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Count_GridWithMultipleOccurrences_ReturnsCorrectCount()
+    {
+        var grid = new Grid<char>(TestInput.Replace("\n", "").ToCharArray(), 10);
+        var word = "SAM".AsSpan();
+
+        var result = XSearch.Count(grid, word);
+
+        result.Should().Be(0); // Update the expected count based on the actual occurrences
+    }
+
+    [Fact]
+    public void Count_GridWithNoOccurrences_ReturnsZero()
+    {
+        var grid = new Grid<char>(TestInput.Replace("\n", "").ToCharArray(), 10);
+        var word = "XYZ".AsSpan();
+
+        var result = XSearch.Count(grid, word);
+
+        result.Should().Be(0);
+    }
+}
